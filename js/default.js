540bebc9feba13bc34ea60c8985412fe0c660736
ebonycitizen <coco_coco87@hotmail.com>
2017-06-19 17:34:36 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,29 +1,30 @@
 var number = Math.floor(Math.random()*3);
-var answer = parseInt(window.prompt('―数当てゲーム― 0~2の数字を入力してください'));
+var answer:
 
 var message;
+
+if(answer === number)
+{
+  message = 'あたり';
+  break;
+}
+else if(answer < number)
+{
+  message = 'もっと大きいです';
+}
+else if(number < answer)
+{
+  message = 'もっと小さいです';
+  answer = parseInt(window.prompt('―数当てゲーム― 0~2の数字を入力してください'));
+}
+else
+{
+  message = '正しい数字を入力してください';
+}
+
 while(answer !== number)
 {
-  if(answer === number)
-  {
-    message = 'あたり';
-    break;
-  }
-  else if(answer < number)
-  {
-    message = 'もっと大きいです';
-    answer = parseInt(window.prompt('―数当てゲーム― 0~2の数字を入力してください'));
-  }
-  else if(number < answer)
-  {
-    message = 'もっと小さいです';
-    answer = parseInt(window.prompt('―数当てゲーム― 0~2の数字を入力してください'));
-  }
-  else
-  {
-    message = '正しい数字を入力してください';
-    answer = parseInt(window.prompt('―数当てゲーム― 0~2の数字を入力してください'));
-  }
-
-  document.getElementById('choice').textContent = message;
+   answer = parseInt(window.prompt('―数当てゲーム― 0~2の数字を入力してください'));
 }
+
+document.getElementById('choice').textContent = message;
