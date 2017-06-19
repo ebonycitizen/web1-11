4db35aed9d117086ea1344b2f3f5b315fe818c0b
ebonycitizen <coco_coco87@hotmail.com>
2017-06-19 17:39:02 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,25 +2,26 @@ var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('―数当てゲーム― 0~2の数字を入力してください'));
 var message;
 
-if(answer === number)
-{
-  message = 'あたり';
-}
-else if(answer < number)
-{
-  message = 'もっと大きいです';
-}
-else if(number < answer)
-{
-  message = 'もっと小さいです';
-}
-else
-{
-  message = '正しい数字を入力してください';
-}
+
 
 while(answer != number)
 {
   answer = parseInt(window.prompt('もう一回'));
+  if(answer === number)
+  {
+    message = 'あたり';
+  }
+  else if(answer < number)
+  {
+    message = 'もっと大きいです';
+  }
+  else if(number < answer)
+  {
+    message = 'もっと小さいです';
+  }
+  else
+  {
+    message = '正しい数字を入力してください';
+  }
   document.getElementById('choice').textContent = message;
 }
